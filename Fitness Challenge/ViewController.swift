8aa15c4aa812f1bb5b7a4dbe694d2520dd58fdea
Kieran Andrews <kieran.andrews@adelaide.edu.au>
2016-06-10 16:42:58 +0930

    Add new loginkit dep

--- Fitness Challenge/ViewController.swift
+++ Fitness Challenge/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import LoginKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        // Setup
+        LoginKitConfig.url = "https://challengecup.club/"
+        LoginKitConfig.loginPath = "api/v1" // TODO: Remove this for basic auth - or create endpoint?
+        LoginKitConfig.authType = AuthType.Basic
+        LoginKitConfig.savedLogin = true
+        //        LoginKitConfig.destination = { ()-> UIViewController in JobViewController() }
+        LoginKitConfig.destination = { () -> UIViewController in ActivityViewController() }
+        LoginKitConfig.logoImage = UIImage(named: "logo") ?? UIImage()
+        
+        LoginKit.Appearance().backgroundColor = UIColor(hue: 0.5694, saturation: 1, brightness: 0.61, alpha: 1.0)
+        
+        let login_screen = LoginKit.loginScreenController() as! LoginController
+        self.presentViewController(login_screen, animated: animated,completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
--- Podfile
+++ Podfile
@@ -3,14 +3,20 @@ use_frameworks!
 
 target 'Fitness Challenge' do
     pod 'IQKeyboardManager'
-    pod 'Rollbar'
+#    pod 'Rollbar' # Disabled until Bitcode is fixed
     pod 'KeychainAccess'
     pod 'GraphKit'
     pod 'SVProgressHUD'
     pod 'SwiftyJSON', '~> 2.3'
-    pod 'Alamofire'
-#    pod 'Localytics'
+#pod 'Alamofire', '~> 2'
+#    pod 'Flurry-iOS-SDK/FlurrySDK'
+    pod 'Mixpanel'
+    pod 'Eureka' #, '~> 1.0'
+    # pod 'Localytics'
+    pod 'LoginKit', path: "~/dev/LoginKit"
 end
+
+
 
 target 'Fitness ChallengeTests' do
 
