3e857c91808f96119cfa7f32d990325fde0f281b
Kieran Andrews <kieran.andrews@adelaide.edu.au>
2016-06-10 16:40:28 +0930

    update xcode files

---
+++ Fitness Challenge/FitnessChallenge-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  FitnessChallenge-Bridging-Header.h
+//  Fitness Challenge
+//
+//  Created by Kieran Andrews on 20/10/2015.
+//  Copyright © 2015 Kieran Andrews. All rights reserved.
+//
+
+#ifndef FitnessChallenge_Bridging_Header_h
+#define FitnessChallenge_Bridging_Header_h
+
+//#import "Flurry.h"
+
+
+#endif /* FitnessChallenge_Bridging_Header_h */
